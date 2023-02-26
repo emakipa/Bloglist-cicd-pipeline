a3777783591b8b7554b1a3199b7373274d93f77f
Esa Mäkipää <esa.makipaa@gmail.com>
2023-02-26 16:16:58 +0200

    Remove extra lines

--- webpack.config.js
+++ webpack.config.js
@@ -10,72 +10,62 @@ const config = (env, argv) => {
     : 'http://localhost:3003'
 
   return {
-  entry: ['regenerator-runtime/runtime.js', './src/index.js'],
-  //entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  devServer: {
-//    static: path.resolve(__dirname, 'dist'),
-    port: 3000,
-    open: true,
-    //historyApiFallback: true,
-    compress: true,
-    proxy: {
-      '/': {
-          target: 'http://localhost:3003',
-          secure: false
-      }
-    //current: {"node":"12.16.1","npm":"6.13.4"})
+    entry: ['regenerator-runtime/runtime.js', './src/index.js'],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: '/',
     },
-    //proxy: {
-      //"/": "http://localhost:3003"
-    //},
-  },
-  devtool: 'source-map',
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html",
-    }),
-    new webpack.DefinePlugin({
-      BACKEND_URL: JSON.stringify(backend_url)
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env'],
-          }
-        },
+    devServer: {
+      port: 3000,
+      open: true,
+      compress: true,
+      proxy: {
+        '/': {
+            target: 'http://localhost:3003',
+            secure: false
+        }
       },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        loaders: ['style-loader', 'css-loader'],
-      }
+    },
+    devtool: 'source-map',
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./public/index.html",
+        filename: "./index.html",
+      }),
+      new webpack.DefinePlugin({
+        BACKEND_URL: JSON.stringify(backend_url)
+      }),
     ],
-  },
-  resolve: {
-    extensions: ["*", ".js", ".jsx"],
-  },
-  //devServer: {
-    //historyApiFallback: true,
-  //},
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ['@babel/preset-react', '@babel/preset-env'],
+            }
+          },
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader",
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          loaders: ['style-loader', 'css-loader'],
+        }
+      ],
+    },
+    resolve: {
+      extensions: ["*", ".js", ".jsx"],
+    },
   }
 };
 
